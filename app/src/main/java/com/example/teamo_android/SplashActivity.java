c8efb3824214b04757e01845f740aa8be3a90595
Seonho Kim <5061ksh@naver.com>
2021-11-20 23:15:27 +0900

    Developed SplashActivity

---
+++ app/src/main/java/com/example/teamo_android/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.teamo_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView splashImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
