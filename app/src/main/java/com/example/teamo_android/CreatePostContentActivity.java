233ffe5b2d8fddfb84e192f7623d15678947178c
Cyma-s <ssol0319@naver.com>
2021-12-02 00:17:26 +0900

    edit : 글쓰기 만드는 중

---
+++ app/src/main/java/com/example/teamo_android/CreatePostContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.teamo_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePostContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post_content);
+    }
+}
--- app/src/main/java/com/example/teamo_android/Team.java
+++ app/src/main/java/com/example/teamo_android/Team.java
@@ -1,6 +1,8 @@
 package com.example.teamo_android;
 
-public class Team {
+import java.io.Serializable;
+
+public class Team implements Serializable {
     private int id;
     private String title;
     private String content;
@@ -9,9 +11,9 @@ public class Team {
     private String subject;
     private String semester;
     private String professor;
-    private int courseClass;
+    private String courseClass;
 
-    Team(int id, String title, String leaderName, int memberNum, String subject, String semester, String professor, int courseClass) {
+    Team(int id, String title, String leaderName, int memberNum, String subject, String semester, String professor, String courseClass) {
         this.id = id;
         this.title = title;
         this.leaderName = leaderName;
@@ -24,7 +26,7 @@ public class Team {
         this.content = makeContent(this.subject, this.semester, this.professor, this.courseClass);
     }
 
-    private String makeContent(String subject, String semester, String professor, int courseClass) {
+    private String makeContent(String subject, String semester, String professor, String courseClass) {
         String content = ("#" + subject + " #" + semester + "학기" + "\n#" + professor + "교수님 " +
                 "#" + courseClass + "분반");
         return content;
@@ -37,16 +39,16 @@ public class Team {
     public String getSubject() { return this.subject; }
     public String getSemester() { return this.semester; }
     public String getProfessor() { return this.professor; }
-    public int getCourseClass() { return this.courseClass; }
+    public String getCourseClass() { return this.courseClass; }
     public String getContent() { return this.content; }
 
     public void setTitle(String title) { this.title = title; }
     public void setSubject(String subject) { this.subject = subject; }
     public void setSemester(String semester) { this.semester = semester; }
     public void setProfessor(String professor) { this.professor = professor; }
-    public void setCourseClass(int num) { this.courseClass = num; }
+    public void setCourseClass(String num) { this.courseClass = num; }
 
-    public void setContent(String subject, String semester, String professor, int courseClass) {
+    public void setContent(String subject, String semester, String professor, String courseClass) {
         makeContent(subject, semester, professor, courseClass);
     }
 
